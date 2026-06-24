afec51ccac60035705c82c2dfdd302c25b8710b7
Karuna Jain <jainkaruna52@gmail.com>
2026-06-24 20:27:08 +0530

    cleanup: remove old Java Spring Boot backend code

--- backend/src/main/java/com/hero/parts/repository/ModelRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hero.parts.repository;
-
-import com.hero.parts.model.Model;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ModelRepository extends JpaRepository<Model, Long> {
-}
--- backend/src/main/java/com/hero/parts/repository/PartSystemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hero.parts.repository;
-
-import com.hero.parts.model.PartSystem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PartSystemRepository extends JpaRepository<PartSystem, Long> {
-}
